6882aff75eb0092c2ac8042c134d201e1a51f7e3
chaithra86 <chaithrangowda97@gmail.com>
2019-01-31 15:12:00 +0530

    Add new files

---
+++ mul.py
@@ -0,0 +1,11 @@
+import csv
+while(True):
+	print("what would you like to do")
+	print("1.add participant\t 2.see partcipant\t 3.exit")
+	
+def add():
+	name=input("enter the name:")
+	event=input("select event\n 1.cs\t 2.google it\t 3.treasure hunt")
+
+def view():
+
